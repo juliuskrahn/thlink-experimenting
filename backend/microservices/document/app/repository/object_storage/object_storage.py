024d7e1a944497ee5bfa1aeb9787eafdf794196f
Julius Krahn <juliuskrahn@outlook.de>
2021-04-25 10:20:56 +0200

    Started implementing object storage

--- backend/microservices/document/app/repository/object_storage/object_storage.py
+++ backend/microservices/document/app/repository/object_storage/object_storage.py
@@ -1,9 +1,35 @@
+import boto3
+import botocore.exceptions
 
 
 class ObjectStorage:
 
-    def get(self, id_):
-        pass
+    def __init__(self):
+        self._client = boto3.client("s3")
+        self._bucket = "DocumentContent"
 
-    def put(self, id_, object_):
-        pass
+    def get(self, id_: str):
+        try:
+            resp = self._client.get_object(
+                Bucket=self._bucket,
+                Key=id_,
+            )
+            return resp["Body"]
+        except botocore.exceptions.ClientError as e:
+            raise
+
+    def put(self, id_: str, body):
+        try:
+            resp = self._client.put_object(
+                Bucket=self._bucket,
+                Key=id_,
+                Body=body,
+            )
+        except botocore.exceptions.ClientError as e:
+            raise
+
+    def delete(self, id_: str):
+        try:
+            resp = self._client.delete_object(Bucket=self._bucket, Key=id_)
+        except botocore.exceptions.ClientError as e:
+            raise
